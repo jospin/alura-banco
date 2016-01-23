0fcd2bcf5b3d0b9d60f5dc9d96c37b9c21c663ba
jospin <lucien.carbonare@gmail.com>
2016-01-23 17:56:26 -0200

    Aula 5 e 6 do Alura princípios fundamentais. Criando classe COnta

---
+++ alura-banco/Conta/ContaC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alura_banco.Conta
+{
+    class ContaC
+    {
+        public int agencia;
+        public int numero;
+        public string titular;
+        public double saldo;
+        public string cpf;
+
+    }
+}
---
+++ alura-banco/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using alura_banco.Conta;
+
+namespace alura_banco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ContaC conta = new ContaC();
+            conta.numero = 1;
+            conta.titular = "Lucien";
+            conta.saldo = 2000;
+
+            MessageBox.Show("Conta é: " + conta.numero + " - " + conta.titular);
+        }
+    }
+}
